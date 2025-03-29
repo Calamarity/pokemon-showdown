2e5e0066f0a02326e46804a3d1aef65e7b31c518
Calamarity <varuunmenon@gmail.com>
2025-03-29 16:23:04 +0530

    Create custom-formats.ts

---
+++ config/custom-formats.ts
@@ -0,0 +1,15 @@
+export const Formats: FormatList = [
+]
+//Lemon hahahah L bozo
+
+{
+		section: "Free-For-All",
+	},
+	{
+		name: "[Gen 8] Free-For-All",
+		mod: 'gen8',
+		gameType: 'freeforall',
+		rated: false,
+		tournamentShow: false,
+		ruleset: ['Standard', 'Sleep Moves Clause', '!Sleep Clause Mod', '!Evasion Items Clause', '!Dynamax Clause'],
+	}
