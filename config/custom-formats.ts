a4dc02b39d4c39aa0256d379c12a1e914948378e
Calamarity <varuunmenon@gmail.com>
2025-03-29 16:57:16 +0530

    Update custom-formats.ts

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -13,4 +13,27 @@ export const Formats: FormatList = [
 		tournamentShow: false,
 		ruleset: ['Standard NatDex', 'Terastal Clause'],
 	}
+
+	{
+		section: "Free-For-All",
+	},
+	{
+		name: "[Gen 9 NatDex] Free-For-All",
+		mod: 'gen9',
+		gameType: 'freeforall',
+		rated: false,
+		tournamentShow: false,
+		ruleset: ['Standard NatDex', 'Terastal Clause'],
+	}
+		{
+		section: "Teams Multi-Battle",
+	},
+	{
+		name: "[Gen 9 Teams 2v2]",
+		mod: 'gen9',
+		gameType: 'multi',
+		rated: false,
+		tournamentShow: false,
+		ruleset: ['Standard NatDex', 'Terastal Clause', 'Max Team Size = 3'],
+	}
 ]
