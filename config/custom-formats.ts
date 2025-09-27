fd389bcad9fbd75a66c000e39adbb811ca9e49e1
Calamarity <varuunmenon@gmail.com>
2025-09-27 23:55:37 +0530

    Update custom-formats.ts

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -1,7 +1,9 @@
 export const Formats: FormatList = [
 
-	{
-		section: "Custom",
+//Lemon hahahah L bozo
+
+{
+		section: "Custom Formats",
 	},
 	{
 		name: "[Gen 7] Multi Random Battle",
@@ -13,16 +15,16 @@ export const Formats: FormatList = [
 		rated: false,
 		ruleset: [
 			'Max Team Size = 3',
-			'Obtainable', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Illusion Level Mod',
-		],
+			'Obtainable', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Illusion Level Mod'],
 	},
 	{
 		name: "[Gen 7] Free-For-All Random Battle",
 		mod: 'gen7',
 		team: 'randomFFA',
 		gameType: 'freeforall',
+		searchShow: false,
 		tournamentShow: false,
 		rated: false,
 		ruleset: ['Obtainable', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Illusion Level Mod'],
 	},
-];
+	]
